0b337f42a6a58a051a35feadeb6c1c1673729eda
Tom Cartwright <tecartwright@gmail.com>
2024-01-16 21:12:22 +0000

    feat: demostrate durability with bad backoff settings

--- app/jobs/create_horizon_record_job.rb
+++ app/jobs/create_horizon_record_job.rb
@@ -1,9 +1,16 @@
 class CreateHorizonRecordJob < ApplicationJob
   queue_as :default
+  BACKOFF_TIMINGS = [1, 2, 4].freeze
 
   def perform(data)
-    HTTParty.post("http://horizon:3011/horizon_cash_transactions", body: {
-      horizon_cash_transaction: data
-    })
+    begin
+      HTTParty.post("http://horizon:3011/horizon_cash_transactions", body: {
+        horizon_cash_transaction: data
+      })
+    rescue SocketError
+      BACKOFF_TIMINGS.each do |i|
+        CreateHorizonRecordJob.set(wait: i.seconds).perform_later(data)
+      end
+    end
   end
 end
